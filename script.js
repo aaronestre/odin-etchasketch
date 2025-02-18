debcb82b40aa1ec54c37d29c2eaa7c70d8fa6f7e
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-18 15:43:32 -0500

    Add random RBG to brush stroke and increasing opacity

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const newGridButton = document.getElementById('new-grid');
 
 let gridSize = 16;
 const GRID_PIXELS = 650 * 650;
+let opacity = 0;
 
 const createGrid = (gridSize) => {
     
+    opacity = 0;
+
     for ( let i = 0; i < gridSize; i++ ) {
         for ( let j = 0; j < gridSize; j++ ) { 
             const div = document.createElement('div');
@@ -19,8 +22,15 @@ const createGrid = (gridSize) => {
 
 }
 
-const handleMouseEnter = (e) => { 
-    e.target.style.backgroundColor = 'black';
+const handleMouseEnter = (e) => {
+    
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    e.target.style.background = `rgb(${red}, ${green}, ${blue})`;
+    e.target.style.opacity = `${opacity / 100}`;
+    opacity += 10;
 }
 
 newGridButton.addEventListener('click', () => {
