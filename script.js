60c5afe8f9f27df713aaae965d576d37732463cb
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-18 15:21:44 -0500

    Create 16x16 grid of divs

---
+++ script.js
@@ -0,0 +1,16 @@
+const gridContainer = document.getElementById('grid-container');
+
+const createGrid = () => {
+    
+    for ( let i = 0; i < 16; i++ ) {
+        for ( let j = 0; j < 16; j++ ) { 
+
+            const div = document.createElement('div');
+            div.classList.add('grid-item');
+            gridContainer.appendChild(div);
+
+        }
+    }
+
+}
+createGrid();
