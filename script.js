284957dd90dc40336635e05935bec3f0a94dad24
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-18 15:34:28 -0500

    Add new grid button to create custom grid sizes

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const gridContainer = document.getElementById('grid-container');
+const newGridButton = document.getElementById('new-grid');
 
-const createGrid = () => {
+let gridSize = 16;
+const GRID_PIXELS = 650 * 650;
+
+const createGrid = (gridSize) => {
     
-    for ( let i = 0; i < 16; i++ ) {
-        for ( let j = 0; j < 16; j++ ) { 
-
+    for ( let i = 0; i < gridSize; i++ ) {
+        for ( let j = 0; j < gridSize; j++ ) { 
             const div = document.createElement('div');
+            div.style.width = `${650 / gridSize}px`;
+            div.style.height = `${650 / gridSize}px`;
             div.classList.add('grid-item');
             div.addEventListener('mouseenter', handleMouseEnter);
             gridContainer.appendChild(div);
-
         }
     }
 
@@ -18,5 +22,12 @@ const createGrid = () => {
 const handleMouseEnter = (e) => { 
     e.target.style.backgroundColor = 'black';
 }
+
+newGridButton.addEventListener('click', () => {
+    gridContainer.innerHTML = '';
+    const size = prompt('Enter the number of rows and columns to create a grid');
+    gridSize = size > 100 ? 100 : size;
+    createGrid(gridSize);
+})
 
 createGrid();
